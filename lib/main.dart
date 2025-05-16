570fae478765c93b6c6a5b3d90add9bc53fc08e9
highlysuspicious <115582079+highlysuspicious@users.noreply.github.com>
2025-05-16 13:01:41 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      home: const HomeActivity(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ChatApp')),
+      body: const Center(child: Text('Hello World')),
+    );
+  }
+}
